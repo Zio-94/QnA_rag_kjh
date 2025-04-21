d7c5afb781d6693c777a565e177705561fdd5748
jiho.kim <jiho.kim9478@gmail.com>
2025-04-21 20:16:39 +0900

    feat: update configuration and data ingestion logic
    - Removed hardcoded model names for LLM_MODEL and GUARD_CLASSIFIER_MODEL in config.
    - Refactored data ingestion process in lifespan.py to streamline dependency initialization and ensure data ingestion occurs on startup.
    - Adjusted data ingestion service to return early if the collection already exists and overwrite is set to False.

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 import pandas as pd
 
 from .config import settings
-from ..dependencies.get_clients import get_vector_store, get_embedder # 수정: 클라이언트 가져오기
+from src.dependencies.common import get_vector_store, get_embedder, get_data_ingestion_service, get_retriever # 수정: 클라이언트 가져오기
 from src.core.config import get_settings
 from src.services.data_ingestion_service import DataIngestionService
 from src.modules.retriever import Retriever
@@ -14,15 +14,14 @@ logger = logging.getLogger(__name__)
 app_state = {}
 settings = get_settings()
 
-async def _load_and_index_data(embedder, vector_store):
+async def _load_and_index_data():
     """
     앱 시작 시 데이터를 로드하고 Vector DB에 인덱싱합니다.
     이미 인덱싱된 경우 건너뛸 수 있도록 간단한 확인 로직 추가 가능.
     """
 
 
-    data_ingestion_service = DataIngestionService(embedder, vector_store)
-    await data_ingestion_service.run_ingestion_pipeline(overwrite=False) 
+ 
 
 
     logger.info("Data ingestion process completed.")
@@ -32,21 +31,17 @@ async def _load_and_index_data(embedder, vector_store):
 async def lifespan(app: FastAPI):
     # === Startup ===
     logger.info("Application startup...")
-    # Initialize Redis client (using app_state or DI)
-    # Initialize ChromaDB client (using app_state or DI)
-    # Load Re-ranker model if enabled (using app_state or DI)
+
     logger.info("Initializing dependencies...")
-    # Ensure vector store client is ready before ingestion
-    vector_store = get_vector_store(settings) 
-    embedder = get_embedder(settings)
+    vector_store = get_vector_store()
+    embedder = get_embedder()
+    retriever = get_retriever()
+    data_ingestion_service = DataIngestionService(embedder, vector_store)
+    await data_ingestion_service.run_ingestion_pipeline(overwrite=True) 
 
-    # Load and index data if needed
-    # await _load_and_index_data(embedder, vector_store)
 
-
-    retriever = Retriever(embedder=embedder, vector_store=vector_store)
-    result = await retriever.retrieve("주문이 취소/반품되면 상품의 재고가 있잖아요. 그 재고가 복구되어서 수가 늘어나나요?")
-    logger.info(result)
+    # result = await retriever.retrieve("주문이 취소/반품되면 상품의 재고가 있잖아요. 그 재고가 복구되어서 수가 늘어나나요?")
+    # logger.info(result)
 
     yield
 
